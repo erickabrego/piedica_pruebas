06ef102ba17b9fcfe6ec4536897b2b67dbd610bf
Erick Abrego <programador2@grupoley.com>
2022-10-18 13:38:46 -0600

    Update main.py

--- custom_purchase_and_sale/controllers/main.py
+++ custom_purchase_and_sale/controllers/main.py
@@ -1,28 +1,41 @@
 from odoo.http import Controller, request, route, Response
+import logging
+import json
+
+_logger = logging.getLogger(__name__)
+
 
 class MainController(Controller):
 
-    @route('/sync-error-orders/<id>', type='json', auth='none')
+    @route('/sync-error-orders/<id>', type='http', auth='none')
     def copy_error_order(self, id, **kwargs):
+        _logger.info("Dentro")
+
+        response = []
+
         if not str(id).isnumeric():
-            return {
+            response.append({
                 'status': 'error',
                 'message': 'El id de la orden de venta debe ser un valor numérico. Valor introducido: %s' % str(id)
-            }
+            })
         order = request.env['sale.order'].sudo().search([('id', '=', id)])
         if not order:
-            return {
+            response.append({
                 'status': 'error',
                 'message': 'No se encontró la orden de venta con el id %s' % id
-            }
-        copy_order = order.sudo().copy_error_order()
-        if copy_order:
-            return {
-                'status': 'success',
-                'message': f'Se realizó la creación de la nueva orden de venta con id {copy_order.get("res_id")}'
-            }
+            })
         else:
-            return {
-                'status': 'error',
-                'message': f'No fue posible crear la nueva orden a partir del error.'
-            }
+            copy_order = order.sudo().copy_error_order()
+            if copy_order:
+                response.append({
+                    'status': 'success',
+                    'message': f'Se realizó la creación de la nueva orden de venta con id {copy_order.get("res_id")}'
+                })
+
+            else:
+                response.append({
+                    'status': 'error',
+                    'message': f'No fue posible crear la nueva orden a partir del error.'
+                })
+
+        return json.dumps(response, indent=2)
