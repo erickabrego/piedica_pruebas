3bfc187f9fdec1ee8de5a821dc68e1c6d00fab37
Brian Becerra <brian@m22.mx>
2022-10-31 13:58:00 -0600

    no. seg. y transp. vacios

--- crm_sync_orders/models/stock_picking.py
+++ crm_sync_orders/models/stock_picking.py
@@ -8,30 +8,28 @@ class Picking(models.Model):
     _inherit = 'stock.picking'
 
 
-#     def button_validate(self):
-#         res = super().button_validate()
-#         if self.sale_id.folio_pedido:
-#             url = f"https://crmpiedica.com/api/api.php?id_pedido={self.sale_id.folio_pedido}&id_etapa=6"
+    def button_validate(self):
+        res = super().button_validate()
+        if self.sale_id.folio_pedido:
+            url = f"https://crmpiedica.com/api/api.php?id_pedido={self.sale_id.folio_pedido}&id_etapa=6"
 
-#             # no. seguimiento
-#             if self.carrier_tracking_ref:
-#                 url += '&guia_rastreo=' + self.carrier_tracking_ref
+            # no. seguimiento
+            url += '&guia_rastreo=' + (self.carrier_tracking_ref if self.carrier_tracking_ref else '')
 
-#             # transportista
-#             if self.carrier_id:
-#                 url += '&transportista=' + self.carrier_id.name
+            # transportista
+            url += '&transportista=' + (self.carrier_id.name if self.carrier_id else '')
 
-#             token = self.env['ir.config_parameter'].sudo().get_param("crm.sync.token")
-#             headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
-#             response = requests.put(url, headers=headers)
-#             self.sale_id.message_post(body=response.content)
-#             crm_status = self.env["crm.status"].search([("code", "=", "6")], limit=1)
+            token = self.env['ir.config_parameter'].sudo().get_param("crm.sync.token")
+            headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+            response = requests.put(url, headers=headers)
+            self.sale_id.message_post(body=response.content)
+            crm_status = self.env["crm.status"].search([("code", "=", "6")], limit=1)
 
-#             if crm_status:
-#                 self.sale_id.write({'estatus_crm': crm_status.id})
-#                 self.sale_id.create_estatus_crm()
+            if crm_status:
+                self.sale_id.write({'estatus_crm': crm_status.id})
+                self.sale_id.create_estatus_crm()
 
-#         return res
+        return res
 
     def p_button_validate(self):
         # Clean-up the context key at validation to avoid forcing the creation of immediate
